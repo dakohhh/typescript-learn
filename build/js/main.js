572213aee35908fddb4c9a3c95cacd9f82dadc41
dakohhh <wiizzydreadmill@gmail.com>
2024-02-15 16:00:38 -0800

    initail commit

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+var isLoading;
+var the43 = Number("23");
+isLoading = true;
+console.log(typeof the43);
+var sum = function (a, b) {
+    return a + b;
+};
+var userInfo = {
+    firstName: "wsdom",
+    lastName: "dakoh",
+    age: 12,
+};
+var firstName = userInfo.firstName;
+console.log({ firstName: firstName });
+// REGEX
+var r = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+let isLoading: boolean
+
+const the43 = Number("23")
+
+
+isLoading = true
+
+console.log(typeof the43)
+
+
+const sum = (a: number, b: number) => {
+
+    return a + b
+
+}
+
+type UserID = string | number
+
+
+interface User {
+    id: UserID
+    name: string
+
+}
+
+let userInfo = {
+    firstName: "wsdom",
+    lastName: "dakoh",
+    age: 12,
+};
+
+
+
+let { firstName } = userInfo
+
+
+
+console.log({firstName})
+  
+
+
+
+
+// REGEX
+
+const r: RegExp = /\w+/g
+
+
+
+
+
+
+
